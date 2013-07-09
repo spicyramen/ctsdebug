618c3efd0642f1a640f3240bcaafe4f38ee58a73
Gonzalo Gasca Meza <gascagonzalo@gmail.com>
2013-07-09 01:41:45 -0500

    Add CLI

--- ctsdebugweb/webfiles/Other/installStatus.do_files/pageheader.js
+++
@@ -1,4 +0,0 @@
-function launchSearch() {
-	var webapp = window.location.pathname.split('/')[1];
-	window.open('/' + webapp + '/searchDoc.do', 'SearchDoc', 'width=670,height=670,toolbar=no,resizable=yes,scrollbars=yes');
-} 
